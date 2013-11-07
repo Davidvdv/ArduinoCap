6b6165e16ba47599e5ce49cd161b137ea0d8948b
David <d.vandevondervoort@gmail.com>
2013-11-07 12:45:42 +0100

    Twee petten geimplementeerd.

--- app.js
+++ app.js
@@ -14,8 +14,11 @@ var rfidReader = new SerialPort("/dev/cu.usbserial-A700e5V1", {
 	parser: serialport.parsers.readline('\n')
 });
 
-// Init for Arduino
-var cap = new SerialPort("/dev/cu.usbmodem24111", {
+var cap1 = new SerialPort("/dev/cu.usbmodem24111", { 
+	baudrate: 19200, // Bautrate of de Arduino
+	parser: serialport.parsers.readline('\n')
+});
+var cap2 = new SerialPort("/dev/cu.usbmodem24141", { 
 	baudrate: 19200, // Bautrate of de Arduino
 	parser: serialport.parsers.readline('\n')
 });
@@ -26,12 +29,21 @@ io.sockets.on('connection', function(socket) {
 		socket.emit('rfid', rfid);
 	});
 
-	cap.on('data', function(raw) {
+	cap1.on('data', function(raw) {
 		if(raw.indexOf('#') == 0) {
 			raw = raw.replace('#', '');
-			socket.emit('soundChanged', raw);
+			socket.emit('cap1SoundChanged', raw);
 		} else {
-			socket.emit('viewAngleChanged', raw);
+			socket.emit('cap1ViewAngleChanged', raw);
+		}
+	});
+
+	cap2.on('data', function(raw) {
+		if(raw.indexOf('#') == 0) {
+			raw = raw.replace('#', '');
+			socket.emit('cap2SoundChanged', raw);
+		} else {
+			socket.emit('cap2ViewAngleChanged', raw);
 		}
 	});
 });
